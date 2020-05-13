228680850608004e7d2855ed02cbf76e1382ee56
ishkurti12 <ishkurti12@gmail.com>
2020-05-13 14:36:38 +0000

    update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -5,7 +5,9 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Easy login");
         
+
         //Slack users login successfully
+        //this line is written from Intellij
     }
 
 }
