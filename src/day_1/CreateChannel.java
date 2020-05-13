6ac079815515c7524f2b36ff7ee6f20b5495e228
Izmir <ishkurti12@gmail.com>
2020-05-13 15:03:02 +0000

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,9 +4,11 @@ public class CreateChannel {
 
     public static void main(String[] args) {
         System.out.println("New Channel");
-        int x=0;
-        if (x==0){
-            System.out.println("equal");
+        String s= "a";
+if(s.contains("a")){
+    System.out.println("contains");
+}else{
+    System.out.println("not");
         }
     }
 }
