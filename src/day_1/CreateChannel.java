1c06d7d0dca70426b026e6b8d9272c3062f36d2e
ishkurti12 <ishkurti12@gmail.com>
2020-05-13 15:04:52 +0000

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,9 @@ public class CreateChannel {
 
     public static void main(String[] args) {
         System.out.println("New Channel");
+        int x=0;
+        if (x==0){
+            System.out.println("equal");
+        }
     }
 }
